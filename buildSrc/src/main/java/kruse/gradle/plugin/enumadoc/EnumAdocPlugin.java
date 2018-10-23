bdbb976cd024e4b9e41a9c965bf16dbb110f1429
Markus Kruse <markus.kruse@tretton37.com>
2018-10-23 14:45:53 +0200

    first working version

---
+++ buildSrc/src/main/java/kruse/gradle/plugin/enumadoc/EnumAdocPlugin.java
@@ -0,0 +1,14 @@
+package kruse.gradle.plugin.enumadoc;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class EnumAdocPlugin implements Plugin<Project> {
+
+  public void apply(Project project) {
+    project.getTasks().create("enumadoc", EnumAdoc.class, (task) -> {
+      task.setEnumPath("test");
+      task.setOutputFile("world");
+    });
+  }
+}
